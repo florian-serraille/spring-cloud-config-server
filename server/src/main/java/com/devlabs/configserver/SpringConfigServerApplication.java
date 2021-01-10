10011750326b71a0765279ed803bf80892941eea
Florian Serraille <florianserraille@gmail.com>
2021-01-10 04:31:19 -0300

    Remove unnecessary property

--- server/src/main/java/com/devlabs/configserver/SpringConfigServerApplication.java
+++ server/src/main/java/com/devlabs/configserver/SpringConfigServerApplication.java
@@ -1,11 +1,8 @@
 package com.devlabs.configserver;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
